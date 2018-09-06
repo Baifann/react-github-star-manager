2c6c5c4bd0478de128f0aee2fef339f77f7a9a85
baifann <baifan@boohee.com>
2018-09-06 09:10:54 +0800

    feature: update item language tag style

--- src/components/star-list-item/star-list-item.jsx
+++ src/components/star-list-item/star-list-item.jsx
@@ -26,15 +26,17 @@ class StarListItem extends Component {
       <div className="star-list-item-container" onClick={this.onClickResItem.bind(this, this.props.item)}>
         <h3 className="title-repo">{this.props.item.full_name}</h3>
         <p className="title-description">{this.props.item.description}</p>
-        
+        <div className="tag-container">
         {
           this.props.item.language && 
           <div className="title-language">{this.props.item.language}</div>
-        }
+        }        
+        </div>
 
         <div className="bottom-container">
           <div className="title-star"><Star className="icon-star"/><span className="title-info">{this.props.item.stargazers_count}</span></div>
           <div className="title-watch"><Watch className="icon-watch"/><span className="title-info">{this.props.item.forks_count}</span></div>
+          <a href={this.props.item.svn_url} className="title-href">View On Github</a>
         </div>
       </div>
     );
